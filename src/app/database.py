1cff7ef40282dca8580f5a5d41c28064320f6080
duribef <diego.uribe.f@gmail.com>
2024-11-19 10:36:59 -0300

    new database connection

--- src/app/database.py
+++ src/app/database.py
@@ -2,41 +2,34 @@ import os
 import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
 import sqlalchemy.ext.declarative as _declarative
-from google.cloud.sql.connector import Connector, IPTypes
+from google.cloud.sql.connector import Connector
 from sqlalchemy.engine import Engine
 
-def init_connection_engine(connector: Connector) -> Engine:
-    # if env var PRIVATE_IP is set to True, use private IP Cloud SQL connections
-    ip_type = IPTypes.PRIVATE if os.getenv("PRIVATE_IP") is True else IPTypes.PUBLIC
-    # if env var DB_IAM_USER is set, use IAM database authentication
-    user, enable_iam_auth = (
-        (os.getenv("DB_IAM_USER"), True)
-        if os.getenv("DB_IAM_USER")
-        else (os.getenv("DB_USER"), False)
-    )
+def init_connection_engine() -> Engine:
+    env = os.getenv("APP_ENV", "dev")  # Default to development if not set
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASS", "")
+    db_name = os.getenv("DB_NAME")
+    instance_connection_name = os.getenv("INSTANCE_CONNECTION_NAME")
 
-    # Cloud SQL Python Connector creator function
+    # Use Cloud SQL Connector
+    connector = Connector()
+    
     def getconn():
-        conn = connector.connect(
-            os.getenv("INSTANCE_CONNECTION_NAME"),
+        return connector.connect(
+            instance_connection_name,
             "pg8000",
             user=user,
-            password=os.getenv("DB_PASS", ""),
-            db=os.getenv("DB_NAME"),
-            ip_type=ip_type,
-            enable_iam_auth=enable_iam_auth,
+            password=password,
+            db=db_name
         )
-        return conn
 
-    SQLALCHEMY_DATABASE_URL = "postgresql+pg8000://"
+    database_url = "postgresql+pg8000://"
+    engine = _sql.create_engine(database_url, creator=getconn)
 
-    engine = _sql.create_engine(SQLALCHEMY_DATABASE_URL, creator=getconn)
     return engine
 
-# initialize Connector object
-connector = Connector()
-engine = init_connection_engine(connector)
 
+engine = init_connection_engine()
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = _declarative.declarative_base()
